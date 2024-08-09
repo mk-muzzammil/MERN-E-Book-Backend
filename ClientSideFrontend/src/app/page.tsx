597f121f66ba03e16c65579f739d3deb13cd7fba
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-08-09 22:16:32 +0500

    banner added to ome page

--- ClientSideFrontend/src/app/page.tsx
+++ ClientSideFrontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Banner from "@/components/Banner/Banner";
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <Banner />
     </>
   );
 }
