b4f588ddbb128a4dc1d35782df6ce4bfdd53e8d5
Muhammad Muzzammil <muzzammil759@gmail.com>
2024-08-11 00:49:17 +0500

    Add dashboard layout along with two different routes with two different pages

--- AdminDashboard/src/router.tsx
+++ AdminDashboard/src/router.tsx
@@ -2,11 +2,24 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardLayout from "./Layouts/DashboardLayout";
+import BooksPage from "./pages/BooksPage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <DashboardLayout />,
+
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <BooksPage />,
+      },
+    ],
   },
   {
     path: "/login",
